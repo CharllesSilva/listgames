a4069bfdb88a0c033c3e249e077dfc4c4800ee77
CharllesSilva <magrohue10@gmail.com>
2023-07-12 23:41:40 -0300

    new endpoint

--- src/main/java/com/charlles/listgames/services/GameService.java
+++ src/main/java/com/charlles/listgames/services/GameService.java
@@ -1,10 +1,12 @@
 package com.charlles.listgames.services;
 
+import com.charlles.listgames.dto.GameDTO;
 import com.charlles.listgames.dto.GameMinDTO;
 import com.charlles.listgames.entities.Game;
 import com.charlles.listgames.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
